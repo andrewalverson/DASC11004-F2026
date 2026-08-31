d88e2f18b126f0a0664ca1714271915a07b62d2b
Andrew Alverson <aja@uark.edu>
2026-08-31 16:18:06 -0500

    first commit

---
+++ python_scripts/hello-world.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# our first print statement, printing a string
+print('hello world')
+
+# now without a newline character
+print('hello world', end='')
+
+# define two variable that we'll print together
+greeting = 'hello'
+name = "andy"
+
+# print a list of variables
+print(greeting, name)
+
+# do some math
+print(9*8)
+
+dinner = 50
+tip_percent = 0.15
+
+total = dinner + dinner*tip_percent
+print("price of dinner: ", dinner)
+print("tip amount: ", dinner*tip_percent)
+print("total amount:", total)
+
+# python cares about whitespace
+    print("beyonce")
+ print("alicia keys")

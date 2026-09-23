c77862b0121d7491a1c7f357a11f753826cb3614
Andrew Alverson <aja@uark.edu>
2026-09-23 13:05:27 -0500

    fully functioning, added line to skip the header

--- python_scripts/parse-presidents.py
+++ python_scripts/parse-presidents.py
@@ -11,6 +11,9 @@ parties    = defaultdict(int) # key = party name, value = party count
 input_file = '../data/presidents.csv'
 
 with open(input_file, 'r') as prez_file:
+    # skip the first line (the header line)
+    next(prez_file)
+
     # loop over the file line-by-line
     for line in prez_file:
         # split each line at the comma to create a list called 'columns'
@@ -23,7 +26,6 @@ with open(input_file, 'r') as prez_file:
         presidents[columns[0]] = columns[1]
         wikis[columns[0]] = columns[2]
         parties[columns[5].strip()] += 1
-
 
 """
 # get stuff from our dictionary
